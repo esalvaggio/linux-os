09955892c1902d0ae42a5051b35a393e09debd05
Salvaggio <elliots2@illinois.edu>
2018-10-20 23:00:55 -0500

    Paging comments and clarity

--- student-distrib/paging.c
+++ student-distrib/paging.c
@@ -2,32 +2,40 @@
 #include "paging.h"
 
 
+/*
+creates a page directory and page table, initializes both to 0,
+creates 2 elements in page directory, one in 4KB and one in 4MB
+creates page table entry (page) in the 4kB page memory
+asm code puts page directory address into cr0
 
+*/
 
 void Paging_Init(){
 
+//we initially set all the values to 0, which marks it all as unusable, then later
+//define the usuable areas
   int i;
   for(i = 0; i<table_size; i++){
       page_directory[i] = 0; //change 0 to correct initialization value
       page_table[i] = 0; //change
   }
-  page_directory[0] = ((uint32_t) page_table) + 0x06 + 0x01;
-  page_directory[1] = 0x00400000 + 0x82 + 0x01;
-  page_table[0xB8] = 0x000B8000 + 0x06 + 0x01;
+  page_directory[0] = ((uint32_t) page_table) + RE_WR + USER + PRESENT_BIT; //4kB
+  page_directory[1] = KERNEL_LOAD_ADDR + PAGE_SIZE + RE_WR + PRESENT_BIT; //Kernel 4MB address
+  page_table[SHIFTED_VGA_ADDR] = VIDEO + RE_WR + USER + PRESENT_BIT; //VGA 4kB address
 
   //Turn on paging, not yet initialized tables
   //input page directory in here
   /* moves page directory addr into cr3,cr4*/
 
-  asm volatile ("               \n\
-                  movl %0, %%eax \n\
-                  movl %%eax, %%cr3 \n\
-                  movl %%cr4, %%eax \n\
-                  orl $0x10, %%eax \n\
-                  movl %%eax, %%cr4 \n\
-                  movl %%cr0, %%eax \n\
+  asm volatile ("                         \n\
+                  movl %0, %%eax          \n\
+                  movl %%eax, %%cr3       \n\
+                  movl %%cr4, %%eax       \n\
+                  orl $0x10, %%eax        \n\
+                  movl %%eax, %%cr4       \n\
+                  movl %%cr0, %%eax       \n\
                   orl  $0x80000001, %%eax \n\
-                  movl %%eax, %%cr0 \n\
+                  movl %%eax, %%cr0       \n\
                   "
                 :             // (no) ouput
                 : "r"(page_directory)   // page_directory as input. r means go through a regster
--- student-distrib/paging.h
+++ student-distrib/paging.h
@@ -4,6 +4,16 @@
 void Paging_Init();
 
 #define table_size 1024
+#define KERNEL_LOAD_ADDR 0x400000
+#define PRESENT_BIT 0x01
+#define SHIFTED_VGA_ADDR 0xB8
+#define USER        0x04
+#define RE_WR       0x02
+#define PAGE_SIZE   0x80
+#define VIDEO       0xB8000
+
+
+
 uint32_t page_directory[1024] __attribute__((aligned (4096)));
 uint32_t page_table[1024] __attribute__((aligned (4096)));
 
