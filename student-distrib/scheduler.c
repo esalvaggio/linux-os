98af42e5769c945a313b10ed33b8cf2594e0df44
Aubry <caubry2@illinois.edu>
2018-12-04 17:53:31 -0600

    added files for scheduler. small framework for an iretls q q

---
+++ student-distrib/scheduler.c
@@ -0,0 +1,40 @@
+#include "scheduler.h"
+#include "terminal.h"
+
+/*
+  This might all be wrong.
+*/
+
+void switch_processes(){
+  /*
+  term_t* terminal = get_curr_terminal();
+  int current_terminal =
+
+  uint32_t entry_point, stack_pointer, ds, cs;
+  entry_point = 0x0;
+  stack_pointer = 0x0;
+  ds = 0x0;
+  cs = 0x0;
+
+  asm volatile ("                         \n\
+                  movw %2, %%ax           \n\
+                  movw %%ax, %%ds         \n\
+                  movw %%ax, %%es         \n\
+                  movw %%ax, %%fs         \n\
+                  movw %%ax, %%gs         \n\
+                  pushl %2                \n\
+                  pushl %1                \n\
+                  pushfl                  \n\
+                  popl %%eax              \n\
+                  orl $0x00000200,%%eax   \n\
+                  pushl %%eax             \n\
+                  pushl %3                \n\
+                  pushl %0                \n\
+                  iret                    \n\
+                  "
+                  :                 // (no) ouput
+                  : "r"(entry_point), "r"(stack_pointer), "r"(ds), "r"(cs)
+                  : "eax","edx"    // clobbered register
+                );
+    */
+}
---
+++ student-distrib/scheduler.h
@@ -0,0 +1,9 @@
+#ifndef SCHEDULER_H_
+#define SCHEDULER_H_
+#include "types.h"
+void switch_processes();
+
+typedef struct process_struct {
+    struct process_struct *next;
+} process_t;
+#endif
